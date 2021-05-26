2715b411f06fc7377fd7cceac15759092c7d7109
SAITEJA <76157095+sai7teja@users.noreply.github.com>
2021-05-27 01:48:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function change_text(){
+    document.getElementById("nameId").innerHTML = "whether the given year is leap year or not.";
+}
+function showInput() {
+        document.getElementById('display').innerHTML = 
+                    document.getElementById("user_input").value;
+    }
